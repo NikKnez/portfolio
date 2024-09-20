f5b599798d91fbf86270ca8cfb2134b39fb3ba03
NikKnez <nknezevic07@gmail.com>
2024-09-20 22:49:10 +0200

    Old school portfolio done

---
+++ data/snippets/snippet.jsx
@@ -0,0 +1,19 @@
+import { db } from './database.js';
+import { Suspense } from 'react';
+
+async function ConferencePage({ slug }) {
+  const conf = await db.Confs.find({ slug });
+  return (
+    <ConferenceLayout conf={conf}>
+      <Suspense fallback={<TalksLoading />}>
+        <Talks confId={conf.id} />
+      </Suspense>
+    </ConferenceLayout>
+  );
+}
+
+async function Talks({ confId }) {
+  const talks = await db.Talks.findAll({ confId });
+  const videos = talks.map(talk => talk.video);
+  return <SearchableVideoList videos={videos} />;
+}
---
+++ data/snippets/snippet.postgre
@@ -0,0 +1,12 @@
+CREATE TABLE weather (
+    city            varchar(80),
+    temp_lo         int,           -- low temperature
+    temp_hi         int,           -- high temperature
+    prcp            real,          -- precipitation
+    date            date
+);
+
+CREATE TABLE cities (
+    name            varchar(80),
+    location        point
+);
